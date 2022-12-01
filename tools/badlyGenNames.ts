9b27d93f19ee6a709bb437d1cb56a4e703a3cad5
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-11-30 20:53:35 -0800

    added a lot more built-in types

--- tools/badlyGenNames.ts
+++ tools/badlyGenNames.ts
@@ -1,5 +1,5 @@
 
-const original = JSON.parse(await Deno.readTextFile('./static/cli.json'))
+const original = JSON.parse(await Deno.readTextFile('./cli.json'))
 
 const final: Record<string, unknown> = {}
 
@@ -7,4 +7,4 @@ for(const key of Object.keys(original)) {
   final[key] = [key]
 }
 
-await Deno.writeTextFile('./static/names.json', JSON.stringify(final, null, 2))
+await Deno.writeTextFile('./names.json', JSON.stringify(final, null, 2))
--- tools/clean.ts
+++ tools/clean.ts
@@ -2,14 +2,19 @@
 async function alphabetizeJSON(file: string) {
   const original = await Deno.readTextFile(file)
   const parsed = JSON.parse(original)
-  const final: Record<string, unknown> = {}
+  const final: Record<string, Record<string, unknown>> = {}
 
   for(const key of Object.keys(parsed).sort()) {
     final[key] = parsed[key]
+
+    // this adds builtIn automatically to cli without repository attached
+    if(!Object.hasOwn(final[key], "repository")) {
+      final[key].builtIn = true
+    }
   }
 
   await Deno.writeTextFile(file, JSON.stringify(final, null, 2))
 }
 
-await alphabetizeJSON('./static/cli.json')
-await alphabetizeJSON('./static/names.json')
+await alphabetizeJSON('./cli.json')
+await alphabetizeJSON('./names.json')
