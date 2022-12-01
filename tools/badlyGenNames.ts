b1832973909aef53defab496fd34b84954b208ea
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-11-30 21:40:28 -0800

    chore: removed outdated tool

--- tools/badlyGenNames.ts
+++
@@ -1,9 +0,0 @@
-const original = JSON.parse(await Deno.readTextFile("./cli.json"));
-
-const final: Record<string, unknown> = {};
-
-for (const key of Object.keys(original)) {
-  final[key] = [key];
-}
-
-await Deno.writeTextFile("./names.json", JSON.stringify(final, null, 2));
